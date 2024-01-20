cf67cfc7f8f8ac84c804ae72db80b34e6e39066e
Diego-Naches <157024600+Diego-Naches@users.noreply.github.com>
2024-01-21 03:17:43 +0600

    Add files via upload

---
+++ src/pages/html/index.tsx
@@ -0,0 +1,20 @@
+// import  './index.css'
+import styles from './index.module.css';
+
+
+const html = () => {
+
+    return(
+        <div>
+            <p>
+            <img className={styles.myImage} src='https://w.forfun.com/fetch/a4/a4a29e945830a09c09c2e85d7bd905d3.jpeg' alt='photo of bird' /> 
+            </p>
+
+            <p>
+                hi
+            </p>
+        </div>
+    )
+}
+
+export default html
